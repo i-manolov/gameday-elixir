c6a52bc7c7b6aa62172aacf640e5102429e9e907
Ivan Manolov <ivanmanolov90@gmail.com>
2019-07-16 23:44:36 -0400

    Add elixir and erlang buildpack versions

--- config/dev.exs
+++ config/dev.exs
@@ -74,3 +74,5 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :gameday, enable_http_workers: true
